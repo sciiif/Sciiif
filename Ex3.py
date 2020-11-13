9c70dba1891e36640bc30a3c83eac3b1292b9317
sciiif <tgugliemetti@gmail.com>
2020-11-12 23:29:53 -0500

    first upload

---
+++ Ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens.") # prints message
+
+print("Hens", 25 + 30 / 6) # divides 30 by 6 then adds 25 for total ammount of Hens
+print("Roosters", 100 - 25 * 3 % 4) # multiples 25 * 3, then modulus 75 % 4, then subtracts 100 - 3
+
+print("Now I will count the eggs:") # prints message
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # order of operations
+
+print("Is it true that 3 + 2 < 5 - 7?") # prints message
+
+print(3 + 2 < 5 - 7) # adds 3 + 2, then 5 - 7 and determines if 3 +2 is less than 5 - 7
+
+print("What is 3 + 2?", 3 + 2) # asks question then adds
+print("What is 5 - 7?", 5 - 7) # asks question then subtracts
+
+print("Oh, that's why it's false.") # prints message
+
+print("How about some more.") # prints message
+
+print("Is it greater?", 5 > -2) # prints message then determines if its greater than
+print("Is it greater or equal?", 5 >= -2) # prints message then determines if its greater than or equal
+print("Is it less or equal?", 5 <= -2) # prints message then determines if its less than or equal to
